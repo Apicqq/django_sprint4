1ce7423083e1ea0667963c94c53ec85be5a2c441
Nikita Smykov <apic@yandex.ru>
2023-07-01 22:28:48 +0300

    PEP8 refactoring

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -6,16 +6,16 @@ from django.views.generic.edit import CreateView
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('blog.urls')),
-                  path('pages/', include('pages.urls')),
-                  path('auth/', include('django.contrib.auth.urls')),
-                  path('auth/registration/', CreateView.as_view(
+            path('admin/', admin.site.urls),
+            path('', include('blog.urls')),
+            path('pages/', include('pages.urls')),
+            path('auth/', include('django.contrib.auth.urls')),
+            path('auth/registration/', CreateView.as_view(
                       template_name='registration/registration_form.html',
                       form_class=UserCreationForm,
                       success_url=reverse_lazy('blog:index'),
-                  ), name='registration')
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+            ), name='registration')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
